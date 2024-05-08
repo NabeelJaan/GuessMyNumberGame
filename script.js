9b52a9b88d7e6c83862e6b85b41c96368b991618
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2024-05-08 10:38:40 +0500

    code optimization + removing repeated code

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = '🥳 Correct Number';
-
-// document.querySelector('.number').textContent = 20;
-// document.querySelector('.score').textContent = 22;
-
-// document.querySelector('.guess').value;
-// console.log(document.querySelector('.guess').value = 23);
-
-
-// Getting Number input from input value.
-
 // Generating a randon number between 1 to 20
-
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+   document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
    const guess = Number(document.querySelector('.guess').value);
    // check the Number
    if (!guess) {
-      document.querySelector('.message').textContent = 'Please Enter a First';
+      displayMessage('Please Enter a First');
    } else if (guess === secretNumber) {
       // Success message
-      document.querySelector('.message').textContent = 'You Guess the Right Number';
+      displayMessage('You Guess the Right Number');
       document.querySelector('.number').textContent = secretNumber;
       // Change the background color
       document.querySelector('body').style.backgroundColor = '#60b343';
@@ -41,11 +31,11 @@ document.querySelector('.check').addEventListener('click', function () {
       // Check the Number is too High
    } else if (guess !== secretNumber) {
       if (score > 1) {
-         document.querySelector('.message').textContent = guess > secretNumber ? 'Number is High' : 'Number is Low';
+         displayMessage(guess > secretNumber ? 'Number is High' : 'Number is Low');
          score--;
          document.querySelector('.score').textContent = score;
       } else {
-         document.querySelector('.message').textContent = 'You lost the game';
+         displayMessage('You lost the game');
          document.querySelector('.score').textContent = 0;
       }
    }
